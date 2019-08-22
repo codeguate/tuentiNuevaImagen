48f10dd1c68690e1361ed5afb8d67df5dbacb074
Antony417 <adieguez417@gmail.com>
2019-08-22 01:31:04 -0600

    [add, more description, js,css,html][index.html,main.js,style.css]

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,35 @@ $(document).ready(function () {
     $("#resultadosInput").keyup(function () { 
         buscar($(this).val())
     });
+    /*CODE ANTONY*/
+    $(".arrow_more").click(function(){
+      $(".col-img").animate({
+        opacity: 0.25,
+        top: "-=50",
+        height: "toggle"
+      }, 500, function() {
+        $(this).removeClass("d-flex");
+        $(this).addClass("d-none");
+        
+        $(".contein_row_2 ").animate({
+          width: "33%"
+        },500,function(){
+          $(".title h4").css("font-size","4em");
+          $(".title_2").css("font-size","4em");
+          $("#more-2").fadeOut();
+          $(".contain_more_data").removeClass("d-none");
+          $(".contain_more_data").addClass("d-flex");
+          $(".contain_more_data").animate({
+            left: "-=66%",
+            opacity: 1,
+            display: "flex"
+          },500);
+        });
+      });
+      
+      //setTimeout(function(){$(".col-img").fadeOut()},1000);
+    });
+    /*END CODE 21/08/19*/
 });
 
 function buscar(text){
